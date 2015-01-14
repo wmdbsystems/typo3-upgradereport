0f6760ad89b72330743237940de818d80aa894a9
Rupert Germann <rupert.germann@wmdb.de>
2015-01-14 17:41:40 +0100

    added Flo's changes to CliInstallView.php

--- Classes/Cli/CliInstallView.php
+++ Classes/Cli/CliInstallView.php
@@ -8,15 +8,50 @@
 
 class Tx_Smoothmigration_Cli_CliInstallView extends \TYPO3\CMS\Install\View\FailsafeView {
 
+	private static $options;
+
+	private static $last;
+
 	public function render(){
-		foreach($this->variables['availableUpdates'] as $availableUpdate){
-			if($availableUpdate['renderNext']){
-				return $availableUpdate;
-				break;
+		if($this->variables['availableUpdates']) {
+			Tx_Smoothmigration_Cli_CliInstallView::$options = $this->variables['availableUpdates'];
+			foreach ($this->variables['availableUpdates'] as $pointer => $availableUpdate) {
+				if ($availableUpdate['renderNext'] && $pointer != Tx_Smoothmigration_Cli_CliInstallView::$last) {
+					$availableUpdate['way'] = 'a';
+					return $availableUpdate;
+					break;
+				}
+			}
+		} else {
+			if(isset($this->variables['wizardData']['identifier'])) {
+				Tx_Smoothmigration_Cli_CliInstallView::$last = $this->variables['wizardData']['identifier'];
+				$result = $this->getNextStep($this->variables['wizardData']['identifier']);
+				return $result;
 			}
 		}
-		var_dump($this->variables);
 		return false;
+	}
+
+	/**
+	 * @param $currentIdentifier
+	 *
+	 * @return null
+	 */
+	private function getNextStep($currentIdentifier) {
+		$next = null;
+		if(is_array(Tx_Smoothmigration_Cli_CliInstallView::$options)) {
+			$reached = 0;
+			foreach(Tx_Smoothmigration_Cli_CliInstallView::$options AS $identifier => $option) {
+				if($reached == 1) {
+					$next = $option;
+					break;
+				}
+				if($currentIdentifier == $identifier) {
+					$reached = 1;
+				}
+			}
+		}
+		return $next;
 	}
 
 }
